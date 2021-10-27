018bd8e10a6e6cb4a76241e0bca74f4cb785e423
DmitriyKrivovyaz <dmitrykrivovyaz@coherentsolutions.com>
2021-10-27 12:59:07 +0300

    added about view

---
+++ client/src/layouts/infoLayout/InfoLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from 'infoLayout.module.css'
+
+function InfoLayout({ theme }) {
+    return (
+        <div className={classes.container} data-theme={theme}>
+            <h3 className={classes.infoTitle}>Info</h3>
+        </div>
+    )
+}
+
+export default InfoLayout
