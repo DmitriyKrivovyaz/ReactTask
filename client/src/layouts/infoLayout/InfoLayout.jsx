15717986b196aa68aca24dd72f4b984b79e40aa3
DmitriyKrivovyaz <dmitrykrivovyaz@coherentsolutions.com>
2021-10-27 13:40:56 +0300

    refactoring

--- client/src/layouts/infoLayout/InfoLayout.jsx
+++ client/src/layouts/infoLayout/InfoLayout.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import classes from 'infoLayout.module.css'
+import React from 'react';
+import classes from 'infoLayout.module.css';
 
 function InfoLayout({ theme }) {
-    return (
-        <div className={classes.container} data-theme={theme}>
-            <h3 className={classes.infoTitle}>Info</h3>
-        </div>
-    )
+  return (
+    <div className={classes.container} data-theme={theme}>
+      <h3 className={classes.infoTitle}>Info</h3>
+    </div>
+  );
 }
 
-export default InfoLayout
+export default InfoLayout;
